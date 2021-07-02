4b29186770941f86180f3588b6383520e33ab388
jts <maintank2020@126.com>
2021-07-02 16:04:04 +0800

    add Nacos config

--- provider/src/main/java/com/jts/service/impl/UserServiceImpl.java
+++ provider/src/main/java/com/jts/service/impl/UserServiceImpl.java
@@ -1,15 +1,26 @@
 package com.jts.service.impl;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
 import com.jts.service.api.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboService;
 
+import java.util.StringJoiner;
+
 @Slf4j
 @DubboService(version = "1.0.0")
 public class UserServiceImpl implements UserService {
+
+    @NacosValue(value = "${provider.id}", autoRefreshed = true)
+    private String providerId = null;
+
     @Override
     public String getUser() {
         log.info("invoke provider getUser");
-        return System.nanoTime()+"_User";
+        StringJoiner res = new StringJoiner("_")
+                .add(providerId)
+                .add(String.valueOf(System.nanoTime()))
+                .add("User");
+        return res.toString();
     }
 }
