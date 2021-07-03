5465526c62774be162d8119c25274f0d06dea1a7
jts <maintank2020@126.com>
2021-07-03 14:55:29 +0800

    update app config

--- provider/src/main/java/com/jts/ProviderApplication.java
+++ provider/src/main/java/com/jts/ProviderApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableDubbo
 public class ProviderApplication {
 
     public static void main(String[] args) {
--- provider/src/main/java/com/jts/service/conf/NacosConfiguration.java
+++ provider/src/main/java/com/jts/service/conf/NacosConfiguration.java
@@ -1,15 +1,17 @@
-/*
 package com.jts.service.conf;
 
 import com.alibaba.nacos.api.annotation.NacosProperties;
+import com.alibaba.nacos.api.config.ConfigType;
 import com.alibaba.nacos.spring.context.annotation.config.EnableNacosConfig;
 import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.StandardEnvironment;
 
 @Configuration
 @EnableNacosConfig(globalProperties = @NacosProperties(serverAddr = "127.0.0.1:8848"))
-@NacosPropertySource(dataId = "provider.yaml", autoRefreshed = true)
+@NacosPropertySource(dataId = "provider.yaml", autoRefreshed = true, type = ConfigType.YAML,
+        before = StandardEnvironment.SYSTEM_PROPERTIES_PROPERTY_SOURCE_NAME,
+        after = StandardEnvironment.SYSTEM_ENVIRONMENT_PROPERTY_SOURCE_NAME)
 public class NacosConfiguration {
 
 }
-*/
